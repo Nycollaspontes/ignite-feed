6e9423b8dd0eb78535534c09dc1cb763092b7a76
Nycollas Pontes <nycollaspontes@gmail.com>
2022-11-24 01:51:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from "./components/Header/Header";
+import { Post } from "./components/Post/Post";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+
+import styles from "./App.module.css";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Nycollas Pontes" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet lacus velit, vitae pharetra magna sollicitudin et. Nunc venenatis magna nec elit tincidunt maximus. Pellentesque laoreet est id ante varius suscipit. Duis sed congue felis." />
+          <Post author="Rodrigo Pereira" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet lacus velit, vitae pharetra magna sollicitudin et. Nunc venenatis magna nec elit tincidunt maximus. Pellentesque laoreet est id ante varius suscipit. Duis sed congue felis." />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
